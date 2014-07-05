21b518ca901499780544a611f2cf492f874b9adc
Andrés Leone <forestrf@gmail.com>
2014-07-05 04:49:24 +0200

    utf-8 header

--- www/php/ipa/creaversionwidget.php
+++ www/php/ipa/creaversionwidget.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=UTF-8');
+
 if(!isset($_POST['widgetID']) || !isset($_POST['accion']) || !isset($_POST['token'])){
 	exit;
 }
--- www/widgetedit.php
+++ www/widgetedit.php
@@ -1,5 +1,7 @@
 <?php
 
+header('Content-Type: text/html; charset=UTF-8');
+
 session_start();
 if(!isset($_SESSION['usuario'])){
 	exit;
