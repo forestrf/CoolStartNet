070a1962285529a5db9e96871f8bb513fe7bfbfb
Andrés Leone <forestrf@gmail.com>
2014-07-13 20:42:57 +0200

    Trabajando en z.php (render de la web del usuario con los widgets).

--- www/z.php
+++ www/z.php
@@ -35,10 +35,7 @@ Un widget debe de contener lo siguiente:
 El javascript dibujará todo el widget, incluido el div que lo contendrá en el body
 El javascript debe de estar contenido en su totalidad en una función anónima
 El javascript tendrá acceso a la API para leer y escribir variables de cualquier widget
-El javascript tendrá accesp a la posición y tamaño indicado y podrá editarlo ya que serán variables accesibles desde la API
-
-Si es necesario css, se escribirá mediante style en el js.
-Si es necesario imágenes, se usará base64 en el js (lo siento...)
+El javascript tendrá acceso a la posición y tamaño indicado y podrá editarlo ya que serán variables accesibles desde la API
 
 
 <?php
@@ -46,11 +43,17 @@ Si es necesario imágenes, se usará base64 en el js (lo siento...)
 // Widgets del usuario
 $widgets_usuario = $db->getWidgetsDelUsuario();
 foreach($widgets_usuario as &$widget){
+	// Pick the correct widget version
 	if($widget['autoupdate'] === '1'){
 		$version = $db->getWidgetDefaultVersion($widget['ID']);
-		var_dump($version);
+		$version = &$version['version'];
 	}
-	print_r($widget);
+	else{
+		$version = $widget['version'];
+	}
+	
+	// Create the html that will call the script
+	echo "<script src=\"widgetfile.php?widgetID={$widget['ID']}&widgetVersion={$version}&name=main.js\"></script>";
 	
 }
 ?>
