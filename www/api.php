5505fb5babd5d4ba9fd63b8cc1eff54a67167d94
Andrés Leone <forestrf@gmail.com>
2014-07-18 03:12:24 +0200

    Added the License information to more files.

--- www/api.php
+++ www/api.php
@@ -1,5 +1,21 @@
 <?php
 
+# This file is part of MyHomePage.
+#
+#	 MyHomePage is free software: you can redistribute it and/or modify
+#	 it under the terms of the GNU Affero General Public License as published by
+#	 the Free Software Foundation, either version 3 of the License, or
+#	 (at your option) any later version.
+#
+#	 MyHomePage is distributed in the hope that it will be useful,
+#	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	 GNU Affero General Public License for more details.
+#
+#	 You should have received a copy of the GNU Affero General Public License
+#	 along with MyHomePage.  If not, see <http://www.gnu.org/licenses/>.
+
+
 // Ignore warnings. They can be harmful for the json response
 error_reporting(E_ALL ^ E_WARNING);
 
