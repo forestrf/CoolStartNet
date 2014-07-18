cb2e17b1a9ae1e5811d56240935d0da10d5f14e6
Andrés Leone <forestrf@gmail.com>
2014-07-18 15:51:15 +0200

    Making the config function for the background widget.

--- widgets/background/main.js
+++ widgets/background/main.js
@@ -48,22 +48,22 @@ var relations = {
 
 
 
-var fondos = [];
-fondos.push({
+var backgrounds = [];
+backgrounds.push({
 	'1':'https://dl.dropboxusercontent.com/u/1630604/imagenes%20b/wallpapers/1391803994183.jpg',
 	'2':'#000000',
 	'3':'e',
 	'4':'m',
 	'5':'n'
 });
-fondos.push({
+backgrounds.push({
 	'1':'https://dl.dropboxusercontent.com/u/1630604/imagenes%20b/wallpapers/1391407706614.jpg',
 	'2':'#000000',
 	'3':'e',
 	'4':'m',
 	'5':'n'
 });
-fondos.push({
+backgrounds.push({
 	'1':'https://dl.dropboxusercontent.com/u/1630604/imagenes%20b/wallpapers/1390952811479.jpg',
 	'2':'#000000',
 	'3':'e',
@@ -71,18 +71,17 @@ fondos.push({
 	'5':'n'
 });
 
-var cantidadFondos = fondos.length;
-var tiempoTransicion = 3000; //ms
+var transitionTime = 3000; //ms
 var delayBackground = 60000; //ms
-var next = parseInt(Math.random()*cantidadFondos);
+var next = parseInt(Math.random() *backgrounds.length);
 
 
-setBackground(fondoDiv, fondos[next]);
+setBackground(fondoDiv, backgrounds[next]);
 
-if(++next > cantidadFondos -1)
+if(++next > backgrounds.length -1)
 	next = 0;
 
-setBackground(fondoDiv2, fondos[next]);
+setBackground(fondoDiv2, backgrounds[next]);
 
 
 // Interval to change backgrounds over time
@@ -90,19 +89,19 @@ setInterval(function(){
 	fondoDiv2.className = fondoDiv2.className.split("background_widget_transparent").join("");
 	
 	setTimeout(function(){
-		setBackground(fondoDiv, fondos[next]);
+		setBackground(fondoDiv, backgrounds[next]);
 		
 		++next;
-		if(next > cantidadFondos-1)
+		if(next > backgrounds.length -1)
 			next = 0;
 		
 		//Preload the next background
 		fondoDiv2.className += "background_widget_transparent";
-		setBackground(fondoDiv2, fondos[next]);
+		setBackground(fondoDiv2, backgrounds[next]);
 		
 		
-	}, tiempoTransicion);
-}, 60000);
+	}, transitionTime);
+}, delayBackground);
 
 
 // Change the style of a div to make it a background with the options of the background variable
@@ -115,8 +114,56 @@ function setBackground(div, background){
 }
 
 
+
+
+
 // Function for the config widgetID. It returns the html object to append on the config window
 var CONFIG_function = function(){
-	var div = document.createElement('div');
-	return div;
+	
+	// Each row of the table will be a td with an input and another td with a description and the save button
+	var table = document.createElement('table');
+	var tbody = document.createElement('tbody');
+	table.appendChild(tbody);
+	
+	
+	// Time between transitions
+	var tr = document.createElement('tr');
+	var td = document.createElement('td');
+	var inputDelayBackground = document.createElement('input');
+	inputDelayBackground.value = delayBackground/1000; // ms to s
+	td.appendChild(inputDelayBackground);
+	tr.appendChild(td);
+	
+	td = document.createElement('td');
+	td.innerHTML = 'Time between transitions (in seconds)';
+	tr.appendChild(td);
+	tbody.appendChild(tr);
+	
+	
+	// Time transitioning (in seconds)
+	tr = document.createElement('tr');
+	td = document.createElement('td');
+	var inputTransitionTime = document.createElement('input');
+	inputTransitionTime.value = transitionTime/1000; // ms to s
+	td.appendChild(inputTransitionTime);
+	tr.appendChild(td);
+	
+	td = document.createElement('td');
+	td.innerHTML = 'Time transitioning (in seconds)';
+	tr.appendChild(td);
+	tbody.appendChild(tr);
+	
+	
+	
+	
+	
+	
+	var inputDelayBackgroundTime = document.createElement('input');
+	inputDelayBackgroundTime.value = transitionTime;
+	
+	
+	
+	
+	
+	return table;
 }
