35b0374fdf503c768c0a08250c902dc60c1c20d3
Andrés Leone <forestrf@gmail.com>
2014-08-28 16:33:42 +0200

    sql database updated

--- sql/forest.sql
+++ sql/forest.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.2.7
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 19, 2014 at 06:02 PM
--- Server version: 5.5.32
--- PHP Version: 5.4.25
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 28, 2014 at 03:31 PM
+-- Server version: 5.5.39
+-- PHP Version: 5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,15 +23,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `access-token`
+--
+
+CREATE TABLE IF NOT EXISTS `access-token` (
+  `IDuser` int(11) NOT NULL,
+  `dropbox_accessToken` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `files`
 --
 
 CREATE TABLE IF NOT EXISTS `files` (
   `hash` varchar(32) COLLATE utf8_bin NOT NULL,
   `data` mediumblob NOT NULL,
-  `mimetype` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT 'text/plain',
-  PRIMARY KEY (`hash`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `mimetype` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT 'text/plain'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -40,13 +50,11 @@ CREATE TABLE IF NOT EXISTS `files` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+`ID` int(11) NOT NULL,
   `nick` varchar(15) COLLATE utf8_bin NOT NULL,
   `password` varchar(32) COLLATE utf8_bin NOT NULL,
-  `RND` varchar(32) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `nick` (`nick`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
+  `RND` varchar(32) COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -58,9 +66,8 @@ CREATE TABLE IF NOT EXISTS `variables` (
   `IDuser` int(11) NOT NULL,
   `IDwidget` int(11) NOT NULL,
   `variable` varchar(30) COLLATE utf8_bin NOT NULL,
-  `value` text COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`IDuser`,`IDwidget`,`variable`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `value` text COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -69,13 +76,11 @@ CREATE TABLE IF NOT EXISTS `variables` (
 --
 
 CREATE TABLE IF NOT EXISTS `widgets` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
+`ID` int(11) NOT NULL,
   `name` varchar(30) COLLATE utf8_bin NOT NULL,
   `ownerID` int(11) NOT NULL,
-  `published` int(11) NOT NULL DEFAULT '-1' COMMENT 'Si se publica cambiar a 0 o + desde php. Nunca volver a -1',
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=6 ;
+  `published` int(11) NOT NULL DEFAULT '-1' COMMENT 'Si se publica cambiar a 0 o + desde php. Nunca volver a -1'
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=10 ;
 
 -- --------------------------------------------------------
 
@@ -87,9 +92,8 @@ CREATE TABLE IF NOT EXISTS `widgets-content` (
   `IDwidget` int(11) NOT NULL,
   `version` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_bin NOT NULL,
-  `hash` varchar(32) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`IDwidget`,`version`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `hash` varchar(32) COLLATE utf8_bin NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -101,9 +105,8 @@ CREATE TABLE IF NOT EXISTS `widgets-user` (
   `IDuser` int(11) NOT NULL,
   `IDwidget` int(11) NOT NULL,
   `autoupdate` tinyint(1) NOT NULL DEFAULT '1',
-  `version` int(11) NOT NULL COMMENT 'Mirar cuando autoupdate = 0',
-  PRIMARY KEY (`IDuser`,`IDwidget`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `version` int(11) NOT NULL COMMENT 'Mirar cuando autoupdate = 0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -117,10 +120,75 @@ CREATE TABLE IF NOT EXISTS `widgets-versions` (
   `public` tinyint(1) NOT NULL COMMENT '0 = privada, 1 = pública',
   `visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 = oculto, 1 = visible',
   `comment` tinytext COLLATE utf8_bin NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`IDwidget`,`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `access-token`
+--
+ALTER TABLE `access-token`
+ ADD PRIMARY KEY (`IDuser`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+ ADD PRIMARY KEY (`hash`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `nick` (`nick`);
+
+--
+-- Indexes for table `variables`
+--
+ALTER TABLE `variables`
+ ADD PRIMARY KEY (`IDuser`,`IDwidget`,`variable`);
+
+--
+-- Indexes for table `widgets`
+--
+ALTER TABLE `widgets`
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Indexes for table `widgets-content`
+--
+ALTER TABLE `widgets-content`
+ ADD PRIMARY KEY (`IDwidget`,`version`,`name`);
+
+--
+-- Indexes for table `widgets-user`
+--
+ALTER TABLE `widgets-user`
+ ADD PRIMARY KEY (`IDuser`,`IDwidget`);
+
+--
+-- Indexes for table `widgets-versions`
+--
+ALTER TABLE `widgets-versions`
+ ADD PRIMARY KEY (`IDwidget`,`version`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `widgets`
+--
+ALTER TABLE `widgets`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
