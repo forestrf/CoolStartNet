9550f0a96f64f168c90cb1c7192af3c9f247d961
Andrés Leone <forestrf@gmail.com>
2014-08-28 13:21:47 +0200

    www.coolstart.net -> coolstart.net Analytics code added

--- www/z.php
+++ www/z.php
@@ -40,6 +40,12 @@ $db = new DB();
 	<link rel="stylesheet" href="css/reset.min.css"/>
 	<script src="js/crel2.js"></script>
 	<script src="js/api.js"></script>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-54264686-1', 'auto');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
 
