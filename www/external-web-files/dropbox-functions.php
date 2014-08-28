a02958337e29cf722d76a5b0bbb00a7cab7886e5
Andrés Leone <forestrf@gmail.com>
2014-08-28 17:47:36 +0200

    Dropbox https

--- www/external-web-files/dropbox-functions.php
+++ www/external-web-files/dropbox-functions.php
@@ -9,7 +9,7 @@ use \Dropbox as dbx;
 function getWebAuth(){
 	$appInfo          = dbx\AppInfo::loadFromJsonFile('../php/private_data/dropbox_app.json');
 	$clientIdentifier = DROPBOX_APP_NAME;
-	$redirectUri      = 'http://'.WEB_PATH.'external-web-files/dropbox-response.php';
+	$redirectUri      = 'https://'.WEB_PATH.'external-web-files/dropbox-response.php';
 	$csrfTokenStore   = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
 	return new dbx\WebAuth($appInfo, $clientIdentifier, $redirectUri, $csrfTokenStore);
 }
