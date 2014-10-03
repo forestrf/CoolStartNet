bbdd16755062f6556e83308e343f354e47c3cf52
Andrés Leone <forestrf@gmail.com>
2014-10-03 02:30:03 +0200

    fix object creation without new keyword

--- www/external-web-files/dropbox-functions.php
+++ www/external-web-files/dropbox-functions.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once dirname(__FILE__).'/../php/config.php';
+require_once __DIR__.'/../php/config.php';
 
 # Include the Dropbox SDK libraries
-require_once dirname(__FILE__).'/../php/lib/Dropbox/autoload.php';
+require_once __DIR__.'/../php/lib/Dropbox/autoload.php';
 use \Dropbox as dbx;
 
 function getWebAuth(){
-	$appInfo          = dbx\AppInfo(DROPBOX_KEY, DROPBOX_SECRET);
+	$appInfo          = new dbx\AppInfo(DROPBOX_KEY, DROPBOX_SECRET);
 	$clientIdentifier = DROPBOX_APP_NAME;
 	$redirectUri      = 'https://'.WEB_PATH.'external-web-files/dropbox-response.php';
 	$csrfTokenStore   = new dbx\ArrayEntryStore($_SESSION, 'dropbox-auth-csrf-token');
