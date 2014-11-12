f69ada454dd7caaa2fac83b1c068633e0d6e4a0e
Andrés Leone <forestrf@gmail.com>
2014-11-13 00:47:28 +0100

    .htaccess php automatic file detection when url not ends in / Developers stuff in process

--- sql/forest.sql
+++ sql/forest.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 04, 2014 at 07:00 PM
+-- Generation Time: Oct 23, 2014 at 02:16 AM
 -- Server version: 5.5.39
 -- PHP Version: 5.4.31
 
@@ -26,10 +26,10 @@ SET time_zone = "+00:00";
 -- Table structure for table `access-token`
 --
 
-CREATE TABLE IF NOT EXISTS `access-token` (
+CREATE TABLE `access-token` (
   `IDuser` int(11) NOT NULL,
   `dropbox_accessToken` text COLLATE utf8_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -37,11 +37,11 @@ CREATE TABLE IF NOT EXISTS `access-token` (
 -- Table structure for table `files`
 --
 
-CREATE TABLE IF NOT EXISTS `files` (
+CREATE TABLE `files` (
   `hash` varchar(32) COLLATE utf8_bin NOT NULL,
   `data` mediumblob NOT NULL,
   `mimetype` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT 'text/plain'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -49,12 +49,12 @@ CREATE TABLE IF NOT EXISTS `files` (
 -- Table structure for table `session_data`
 --
 
-CREATE TABLE IF NOT EXISTS `session_data` (
+CREATE TABLE `session_data` (
   `session_id` varchar(32) NOT NULL DEFAULT '',
   `hash` varchar(32) NOT NULL DEFAULT '',
   `session_data` blob NOT NULL,
   `session_expire` int(11) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `session_data` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
 `ID` int(11) NOT NULL,
   `nick` varchar(15) COLLATE utf8_bin NOT NULL,
   `password` varchar(32) COLLATE utf8_bin NOT NULL,
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `validation` varchar(5) COLLATE utf8_bin NOT NULL,
   `recover_code_due_date` date NOT NULL,
   `creation_date` date NOT NULL
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=62 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=62 ;
 
 -- --------------------------------------------------------
 
@@ -80,12 +80,12 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `variables`
 --
 
-CREATE TABLE IF NOT EXISTS `variables` (
+CREATE TABLE `variables` (
   `IDuser` int(11) NOT NULL,
   `IDwidget` int(11) NOT NULL,
   `variable` varchar(30) COLLATE utf8_bin NOT NULL,
   `value` text COLLATE utf8_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -93,12 +93,12 @@ CREATE TABLE IF NOT EXISTS `variables` (
 -- Table structure for table `widgets`
 --
 
-CREATE TABLE IF NOT EXISTS `widgets` (
+CREATE TABLE `widgets` (
 `ID` int(11) NOT NULL,
   `name` varchar(30) COLLATE utf8_bin NOT NULL,
   `ownerID` int(11) NOT NULL,
   `published` int(11) NOT NULL DEFAULT '-1' COMMENT 'Si se publica cambiar a 0 o + desde php. Nunca volver a -1'
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=14 ;
 
 -- --------------------------------------------------------
 
@@ -106,12 +106,12 @@ CREATE TABLE IF NOT EXISTS `widgets` (
 -- Table structure for table `widgets-content`
 --
 
-CREATE TABLE IF NOT EXISTS `widgets-content` (
+CREATE TABLE `widgets-content` (
   `IDwidget` int(11) NOT NULL,
   `version` int(11) NOT NULL,
   `name` varchar(50) COLLATE utf8_bin NOT NULL,
   `hash` varchar(32) COLLATE utf8_bin NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -119,12 +119,12 @@ CREATE TABLE IF NOT EXISTS `widgets-content` (
 -- Table structure for table `widgets-user`
 --
 
-CREATE TABLE IF NOT EXISTS `widgets-user` (
+CREATE TABLE `widgets-user` (
   `IDuser` int(11) NOT NULL,
   `IDwidget` int(11) NOT NULL,
   `autoupdate` tinyint(1) NOT NULL DEFAULT '1',
   `version` int(11) NOT NULL COMMENT 'Mirar cuando autoupdate = 0'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -132,14 +132,14 @@ CREATE TABLE IF NOT EXISTS `widgets-user` (
 -- Table structure for table `widgets-versions`
 --
 
-CREATE TABLE IF NOT EXISTS `widgets-versions` (
+CREATE TABLE `widgets-versions` (
   `IDwidget` int(11) NOT NULL,
   `version` int(11) NOT NULL,
   `public` tinyint(1) NOT NULL COMMENT '0 = privada, 1 = pública',
   `visible` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0 = oculto, 1 = visible',
   `comment` tinytext COLLATE utf8_bin NOT NULL,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Indexes for dumped tables
@@ -173,13 +173,13 @@ ALTER TABLE `users`
 -- Indexes for table `variables`
 --
 ALTER TABLE `variables`
- ADD PRIMARY KEY (`IDuser`,`IDwidget`,`variable`);
+ ADD PRIMARY KEY (`IDuser`,`IDwidget`,`variable`), ADD KEY `variables IDwidget` (`IDwidget`);
 
 --
 -- Indexes for table `widgets`
 --
 ALTER TABLE `widgets`
- ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `name` (`name`);
+ ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `name` (`name`), ADD KEY `ownerID` (`ownerID`);
 
 --
 -- Indexes for table `widgets-content`
@@ -191,7 +191,7 @@ ALTER TABLE `widgets-content`
 -- Indexes for table `widgets-user`
 --
 ALTER TABLE `widgets-user`
- ADD PRIMARY KEY (`IDuser`,`IDwidget`);
+ ADD PRIMARY KEY (`IDuser`,`IDwidget`), ADD KEY `widgets-user IDwidget` (`IDwidget`);
 
 --
 -- Indexes for table `widgets-versions`
@@ -213,6 +213,50 @@ MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=62;
 --
 ALTER TABLE `widgets`
 MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `variables`
+--
+ALTER TABLE `variables`
+ADD CONSTRAINT `variables IDwidget` FOREIGN KEY (`IDwidget`) REFERENCES `widgets` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `variables IDuser` FOREIGN KEY (`IDuser`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `widgets`
+--
+ALTER TABLE `widgets`
+ADD CONSTRAINT `widget owner` FOREIGN KEY (`ownerID`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `widgets-content`
+--
+ALTER TABLE `widgets-content`
+ADD CONSTRAINT `widgets-content IDwidget` FOREIGN KEY (`IDwidget`) REFERENCES `widgets` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `widgets-user`
+--
+ALTER TABLE `widgets-user`
+ADD CONSTRAINT `widgets-user IDuser` FOREIGN KEY (`IDuser`) REFERENCES `users` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `widgets-user IDwidget` FOREIGN KEY (`IDwidget`) REFERENCES `widgets` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `widgets-versions`
+--
+ALTER TABLE `widgets-versions`
+ADD CONSTRAINT `widgets-versions IDwidget` FOREIGN KEY (`IDwidget`) REFERENCES `widgets` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+
+
+
+-- This will allow global variables
+INSERT INTO `forest`.`users` (`ID`, `nick`, `password`, `email`, `RND`, `level`, `validation`, `recover_code_due_date`, `creation_date`) VALUES ('0', 'global', '-', '-', '-', '0', '', '', '');
+INSERT INTO `forest`.`widgets` (`ID`, `name`, `ownerID`, `published`) VALUES ('-1', 'global', '0', '-1');
--- www/developers.php
+++ www/developers.php
@@ -27,39 +27,51 @@
 		GitHub
 		*/
 		
-		var own_widgets_list, developers_menu, developers_back;
+		var widgets_menu, own_widgets_list, developers_menu, developers_back, widgets_back;
 		
 		C(div,
 			C("div", ["class", "left panel"],
-				C("div", ["class", "create"],
-					C("input", ["class", "name", "placeholder", "Widget name"]),
-					C("input", ["class", "button", "type","button", "value", "Create widget"])
+				widgets_menu = C("div",
+					C("div", ["class", "create"],
+						C("input", ["class", "name", "placeholder", "Widget name"]),
+						C("input", ["class", "button", "type","button", "value", "Create widget"])
+					),
+					C("div", ["class", "manage"],
+						"Manage your own widgets",
+						own_widgets_list = C("div", ["class", "widget_list"])
+					)
 				),
-				C("div", ["class", "manage"],
-					"Manage your own widgets",
-					own_widgets_list = C("div", ["class", "widget_list"])
-				)
+				C("div", ["class", "links"],
+					widgets_back = C("a", ["style", "display:none", "onclick", widgs_back], "Back")
+				),
+				widgets_space = C("div", ["class", "widgets_space"])
 			),
 			C("div", ["class", "right panel"],
 				C("div", ["class", "developers_menu"],
 					C("div", "Developer's FAQ"),
-					developers_menu = C("ul",
-						C("li", C("a", ["href", "doc/reader.html", "onclick", show_docs], "Documentation")),
-						C("li", C("a", ["href", "//..."], "How to start")),
-						C("li", C("a", ["href", "//..."], "Limitations (What you can't do)")),
-						C("li", C("a", ["href", "//..."], "Benefits (What are you able to do)")),
-						C("li", C("a", ["href", "//..."], "Good practices"))
-					),
-					developers_back = C("a", ["style", "display:none", "onclick", devs_back], "Back")
+					C("div", ["class", "links"],
+						developers_menu = C("ul",
+							C("li", C("a", ["href", "doc/reader.html", "onclick", show_docs], "Documentation")),
+							C("li", C("a", ["href", "//..."], "How to start")),
+							C("li", C("a", ["href", "//..."], "Limitations (What you can't do)")),
+							C("li", C("a", ["href", "//..."], "Benefits (What are you able to do)")),
+							C("li", C("a", ["href", "//..."], "Good practices"))
+						),
+						developers_back = C("a", ["style", "display:none", "onclick", devs_back], "Back")
+					)
 				),
 				developers_space = C("div", ["class", "developers_space"])
 			)
 		);
 		
-		function show_docs() {
-			developers_space.appendChild(C("iframe", ["src", "doc/reader.html", "class", "doc_iframe"]));
-			developers_menu.style.display = "none";
-			developers_back.style.display = "";
+		function show_docs(path) {
+			if (path === undefined || typeof path !== "string") {
+				path = "";
+			}
+			devs_back();
+			devs_fwd();
+			
+			developers_space.appendChild(C("iframe", ["src", "doc/reader.html#" + path, "class", "doc_iframe"]));
 			return false;
 		}
 		
@@ -69,6 +81,49 @@
 			developers_space.innerHTML = "";
 			developers_back.style.display = "none";
 		}
+		
+		function devs_fwd() {
+			developers_menu.style.display = "none";
+			developers_back.style.display = "";
+		}
+		
+		
+		
+		
+		
+		
+		function widgs_back() {
+			widgets_menu.style.display = "";
+			widgets_space.style.display = "";
+			widgets_space.innerHTML = "";
+			widgets_back.style.display = "none";
+		}
+		
+		function widgs_fwd() {
+			widgets_menu.style.display = "none";
+			widgets_back.style.display = "";
+		}
+		
+		
+		
+		
+		function manage_widget(data){
+			widgs_back();
+			widgs_fwd();
+			
+			C(widgets_space,
+				C("input", ["type", "text", "value", data.name, "name", "name"]), "Name", C("br"),
+				C("input", ["type", "text", "value", data.description, "name", "description"]), "Description", C("br"),
+				C("input", ["type", "file", "value", data.image, "name", "image"]), "Image", C("br"),
+				C("input", ["type", "text", "value", data.tags, "name", "tags"]), "Tags (Comma-separated)", C("br"),
+				"Versions", C("br")
+			);
+			
+			console.log(data);
+			
+			widgets_space.appendChild(C("div"));
+		}
+		
 		
 		
 		
@@ -80,13 +135,25 @@
 			'',
 			function (data) {
 				data = JSON.parse(data);
-				if (data.status = "OK") {
+				if (data.status === "OK") {
 					for (var i = 0, l = data.response.length; i < l; i++) {
-						own_widgets_list.appendChild(generate_widget_element(data.response[i]));
+						own_widgets_list.appendChild(generate_widget(data.response[i]));
 					}
 				}
 			}
 		);
+		
+		function generate_widget(data) {
+			var widget = generate_widget_element(data);
+			
+			widget.appendChild(
+				C("div", ["class", "settings", "onclick", function(){
+					manage_widget(data)
+				}], "M A N A G E")
+			);
+			
+			return widget;
+		}
 		
 		
 		
@@ -100,5 +167,5 @@
 	$html = ob_get_contents();
 	ob_end_clean();
 	
-	echo render_wrapper('Manage widgets - CoolStart.net', $html, false);
+	echo render_wrapper('Developers panel - CoolStart.net', $html, false);
 ?>
